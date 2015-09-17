21e0308074e43e80dd169c391f56da93af919f33
mk <mk@mk.mk>
2015-09-17 22:59:25 +0200

    commit message

--- a/a/main.cpp
+++ a/a/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 using namespace std;
 int main(int argc, char**argv)
 {
 	cout << "hello 223345678910111213\n";
 	cout <<"modified with sourceTree";
 	cout << "aaa";
-
+	cout << "lublin";
 }
